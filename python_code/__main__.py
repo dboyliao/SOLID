e5f3794619f7f3066568bfbf771603103fa12457
dboyliao <qmalliao@gmail.com>
2017-04-15 15:41:34 +0800

    Python3 support

--- python_code/__main__.py
+++ python_code/__main__.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from good import __all__ as all_submodules
+from __future__ import print_function, absolute_import
+from .good import __all__ as all_submodules
 import sys
 
 def main(argv):
     if len(argv) > 1 and argv[1] in ["-l", "--list"]:
-        print "Available submodules: "
+        print("Available submodules: ")
         for i, submodule in enumerate(all_submodules, 1):
-            print "{}. good.{} / bad.{}".format(i, submodule, submodule)
+            print("{}. good.{} / bad.{}".format(i, submodule, submodule))
 
 if __name__ == "__main__":
-    
+
     main(sys.argv)
